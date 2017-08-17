e8046b6c88cbdf586a1a9b186ed800aae4b47836
florence.massen <florence@massen.eu>
2017-08-17 09:45:10 +0200

    new best result 2nd commit

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,7 +2,7 @@ import click
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import IsolationForest
-
+from sklearn.preprocessing import FunctionTransformer
 
 
 def read_raw_data(input_filepath):
@@ -19,12 +19,29 @@ def drop_columns(df, todrop):
     return df.drop(todrop, axis=1)
     
 
-def transform_ordinals(df, ordinals, mapping):        
+def transform_ordinals(df):        
     """
     Replaces values in df on columns in list ordinals following 
     mapping provided
     """
-    return df[ordinals].replace(mapping)
+    # transform ordinal features represented as categories to numeric
+    # this allows the model to reason about the distance between different values
+    ordinals = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'HeatingQC',
+               'KitchenQual', 'FireplaceQu', 'GarageQual', 'GarageCond', 'PoolQC']
+    
+    # First uniformize the set of string values
+    valmap = {'Mn': 'Po', 'No': np.nan}     
+    df[ordinals] = df[ordinals].replace(valmap)
+    
+    # replace the NA values with an empty string so they can be used as key
+    # account in the next mapping
+    df[ordinals] = df[ordinals].fillna('')
+    
+    valmap = {'': 0, 'Po': 1, 'Fa': 2, 'Av': 3, 'TA': 3, 'Gd': 4, 'Ex': 5} 
+    df[ordinals] = df[ordinals].replace(valmap)
+    df[ordinals] = df[ordinals].astype(int)
+
+    return df
     
 
 def transform_categoricals(df):
@@ -32,10 +49,49 @@ def transform_categoricals(df):
     Returns view of df where categorical features have been replaced by 
     multiple features using One Hot Encoding
     """
+    df['MSSubClass'] = df['MSSubClass'].astype(str)
+    oldcolumns = df.columns
+    categoricals = df.select_dtypes(include=['object', 'category']).columns    
     df = pd.get_dummies(df)
+    cat_map = {}
+    newcolumns = list(set(df.columns) - set(oldcolumns))
+    for cat in categoricals:
+        categories = filter(lambda x: x.startswith(str(cat+"_")), newcolumns)
+        cat_map[cat] = list(categories)
+    return df, cat_map
+
+
+def correct_garageyrblt(df):
+    # default NAs to 0   
+    df['GarageYrBlt'] = df['GarageYrBlt'].fillna(0)
+
+    # correct the garage years
+    df['GarageYrBlt'] = np.where((df['YearBuilt'] > df['GarageYrBlt']) & 
+      (df['GarageYrBlt'] > 0), df['YearBuilt'], df['GarageYrBlt'])
+    df['GarageYrBlt'] = df['GarageYrBlt'].astype(int)
     return df
 
+def aggregate_bsmt(df):
+    df['BsmtFinSF'] = df['BsmtFinSF1'] + df['BsmtFinSF2']
+    df = df.drop(['TotalBsmtSF', 'BsmtFinSF1', 'BsmtFinSF2'], axis=1)
+    return df    
 
+def revert_to_original(transformed_df, categorical_map, original_columns):
+    #https://tomaugspurger.github.io/categorical-pipelines.html
+    series = []
+    categorical = [k for k in categorical_map.keys()]
+    non_categorical = list(set(original_columns) - set(categorical))    
+    for col, dums in categorical_map.items():    
+        code_dict = {k:v for (v,k) in enumerate(dums)}        
+        categories = transformed_df[dums].idxmax(axis=1)
+        codes = [code_dict[k] for k in categories]
+        cats = pd.Categorical.from_codes(codes, [d[len(col)+1:len(d)] for d in dums])
+        series.append(pd.Series(cats, name=col))    
+    cat_df = pd.DataFrame(series).T
+    df = pd.concat([transformed_df[non_categorical], cat_df], axis=1)        
+    df = df[original_columns]
+    return df        
+        
 def reduce_categoricals(df, categoricals):
     """
     Perform some binning operation to reduce the number of values 
@@ -44,41 +100,31 @@ def reduce_categoricals(df, categoricals):
     #TBC
     return
     
+def log_of_target(X):
+    return np.log1p(X['SalePrice'])
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-@click.option('--mode', type=click.Choice(['test', 'train']))
+@click.option('--mode', type=click.Choice(['test', 'train', 'eval']))
 def main(input_filepath, output_filepath, mode):
 
     # read original data into data frame
     df = read_raw_data(input_filepath)
       
-    # transform ordinal features represented as categories to numeric
-    # this allows the model to reason about the distance between different values
-    ordinals = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'HeatingQC',
-               'KitchenQual', 'FireplaceQu', 'GarageQual', 'GarageCond', 'PoolQC']
-    
-    # First uniformize the set of string values
-    valmap = {'Mn': 'Po', 'No': np.nan}     
-    df[ordinals] = transform_ordinals(df, ordinals, valmap)
-    
-    # replace the NA values with an empty string so they can be used as key
-    # account in the next mapping
-    df[ordinals] = df[ordinals].fillna('')
-    
-    valmap = {'': 0, 'Po': 1, 'Fa': 2, 'Av': 3, 'TA': 3, 'Gd': 4, 'Ex': 5} 
-    df[ordinals] = transform_ordinals(df, ordinals, valmap)
-    df[ordinals] = df[ordinals].astype(int)
+    #original_columns = df.columns
+        
+    # represent ordinal features as numeric feature to allow
+    # ordering between them    
+    df = transform_ordinals(df)
 
     # perform OneHotEncoding on categorical variables
-    df['MSSubClass'] = df['MSSubClass'].astype(str)
-    df = transform_categoricals(df)
+    df, cat_map = transform_categoricals(df)
+    
+    df = aggregate_bsmt(df)
         
     # correct the garage years
-    df['GarageYrBlt'] = np.where((df['YearBuilt'] > df['GarageYrBlt']) & 
-      (df['GarageYrBlt'] > 0), df['YearBuilt'], df['GarageYrBlt'])
-    df['GarageYrBlt'] = df['GarageYrBlt'].astype(int)
+    df = correct_garageyrblt(df)
 
     # default NAs to 0   
     df = df.fillna(0)
@@ -89,12 +135,19 @@ def main(input_filepath, output_filepath, mode):
         clf.fit(df)
         outlier = clf.predict(df)    
         df = df[outlier == 1]    
-
+        
+#    if mode == 'train':
+#        df['SalePrice'] = np.log1p(df['SalePrice'])
+#    df['LotArea'] = np.log1p(df['LotArea'])
+#    df['LotFrontage'] = np.log1p(df['LotFrontage'])
+# 
     # save the result of this processing as interim data
     df.to_csv(output_filepath, index=False)
-    
 
+#    original = revert_to_original(df, cat_map, original_columns)
     
+#    original.to_csv(output_filepath, index=False)
+        
 
 if __name__ == '__main__':   
     main()
--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,7 +3,10 @@ import csv
 import numpy as np
 import pandas as pd
 import train_model
-
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import Lasso
 
 
 @click.command()
@@ -29,10 +32,44 @@ def main(train_filepath, test_filepath, predict_filepath):
 
     # make sure column order is the same for training and test dataset
     X_test = X_test[X_train.columns]
+
+    features = ['LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', 'YearBuilt',
+       'YearRemodAdd', 'MasVnrArea', 'ExterQual', 'BsmtQual', 'BsmtExposure',
+       'BsmtUnfSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'KitchenQual',
+       'FireplaceQu', 'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF',
+       'OpenPorchSF', 'ScreenPorch', 'MSZoning_C (all)',
+       'Neighborhood_Crawfor', 'Neighborhood_StoneBr', 'Condition1_Norm',
+       'Exterior1st_BrkFace', 'Functional_Typ', 'SaleType_New',
+       'SaleCondition_Abnorml', 'BsmtFinSF']
+
+    gbr = GradientBoostingRegressor(max_depth=4)
+    gbr.fit(X_train[features], y_train)
+    gbr_prediction = gbr.predict(X_test[features])
+
+    lasso = Lasso(alpha=0.01, max_iter=50000)
+    lasso.fit(X_train[features], y_train)
+    lasso_prediction = lasso.predict(X_test[features])
+
+    prediction = []
+    for g,l in zip(gbr_prediction, lasso_prediction):
+        prediction.append(0.1*l + 0.9*g)
+        
+        #0.3 0.7 - 1708-lasso02 - 0.12963
+        #0.2 0.8 - 1708-lasso03 - 0.12916
+        #0.1 0.9 - 1708-lasso04 - 0.13056
+        
+#
+#    rfegbr = GradientBoostingRegressor()
+#    rfe = RFE(estimator=rfegbr, n_features_to_select=10, step=1)    
+#    gbr = GradientBoostingRegressor(max_depth=4, learning_rate=0.1)
+#    pipe = Pipeline([("rfe", rfe), ("gbr", gbr)])
+#    pipe.fit(X_train, y_train)
+#    prediction = pipe.predict(X_test)
+
     
     # build, fit and predict
-    model = train_model.build_fit_model(X_train.drop('Id', axis=1), y_train)
-    prediction = model.predict(X_test.drop('Id', axis=1))
+#    model = train_model.build_fit_model(X_train.drop('Id', axis=1), y_train)
+#    prediction = model.predict(X_test.drop('Id', axis=1))
     
     # write prediction to output file
     result = zip(X_test['Id'], prediction)            
@@ -41,6 +78,7 @@ def main(train_filepath, test_filepath, predict_filepath):
         writer.writerow(["Id", "SalePrice"])
         for Id, pred in result:
             writer.writerow([Id, pred])
+            print("ID: {}, P: {}".format(Id, pred))
             
 
 if __name__ == '__main__':   
